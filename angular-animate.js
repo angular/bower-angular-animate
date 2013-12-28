c7feaa886a43852889b9071d6dcb9b63e097519c
jenkins <ci-bot@angularjs.org>
2013-12-27 23:08:52 -0800

    v1.2.7-build.2028+sha.498365f

--- angular-animate.js
+++ angular-animate.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.7-build.2027+sha.056c849
+ * @license AngularJS v1.2.7-build.2028+sha.498365f
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
