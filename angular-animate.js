06abae21d4254980be1611eb2aaf1153a67628a5
jenkins <ci-bot@angularjs.org>
2014-02-21 12:49:00 -0800

    v1.2.14-build.2304+sha.2f45133

--- angular-animate.js
+++ angular-animate.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.14-build.2303+sha.0c9abc3
+ * @license AngularJS v1.2.14-build.2304+sha.2f45133
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
