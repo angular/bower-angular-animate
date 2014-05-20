e4a91e1b5cb3efb4216b632862a61d346d64a84b
jenkins <ci-bot@angularjs.org>
2014-05-20 14:54:02 -0700

    v1.3.0-build.2721+sha.def5b57

--- angular-animate.js
+++ angular-animate.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.2720+sha.6b52848
+ * @license AngularJS v1.3.0-build.2721+sha.def5b57
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -801,7 +801,7 @@ angular.module('ngAnimate', ['ng'])
          * | 8. The animation ends and all generated CSS classes are removed from the element                                                     | class="my-animation"                                                                 |
          * | 9. The doneCallback() callback is fired (if provided)                                                                                | class="my-animation"                                                                 |
          *
-         * @param {DOMElement} element the element which will it's CSS classes changed
+         * @param {DOMElement} element the element which will its CSS classes changed
          *   removed from it
          * @param {string} add the CSS classes which will be added to the element
          * @param {string} remove the CSS class which will be removed from the element
