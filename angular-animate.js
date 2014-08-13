174770760e1fcf55de679c9d0cdc53388e161e3e
jenkins <ci-bot@angularjs.org>
2014-08-13 14:29:44 -0700

    v1.3.0-build.3044+sha.77a1acc

--- angular-animate.js
+++ angular-animate.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3043+sha.56bd037
+ * @license AngularJS v1.3.0-build.3044+sha.77a1acc
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
