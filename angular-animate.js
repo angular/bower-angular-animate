86aa31bf43a1f31ee985d1cb02e9f6ec42cbf084
jenkins <ci-bot@angularjs.org>
2015-02-12 13:42:54 -0800

    v1.3.14-build.47+sha.140d149

--- angular-animate.js
+++ angular-animate.js
@@ -1,6 +1,6 @@
 /**
- * @license AngularJS v1.4.0-build.3826+sha.c9a4421
- * (c) 2010-2015 Google, Inc. http://angularjs.org
+ * @license AngularJS v1.3.14-build.47+sha.140d149
+ * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
 (function(window, angular, undefined) {'use strict';
