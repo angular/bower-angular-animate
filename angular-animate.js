9a7096cd996979bd132dfece260a517adb9b89b1
jenkins <ci-bot@angularjs.org>
2015-03-01 06:38:43 -0800

    v1.4.0-build.3866+sha.410f7c6

--- angular-animate.js
+++ angular-animate.js
@@ -1,6 +1,6 @@
 /**
- * @license AngularJS v1.3.15-build.62+sha.3a09312
- * (c) 2010-2014 Google, Inc. http://angularjs.org
+ * @license AngularJS v1.4.0-build.3866+sha.410f7c6
+ * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
 (function(window, angular, undefined) {'use strict';
