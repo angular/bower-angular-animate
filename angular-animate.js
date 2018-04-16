d58b58df7b1c313a37f761829824cae71ced7261
jenkins <ci-bot@angularjs.org>
2018-04-16 13:03:14 -0700

    v1.6.10-build.5552+sha.5b7e4b4

--- angular-animate.js
+++ angular-animate.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.9
+ * @license AngularJS v1.6.10-build.5552+sha.5b7e4b4
  * (c) 2010-2018 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -4139,7 +4139,7 @@ angular.module('ngAnimate', [], function initAngularHelpers() {
   isFunction  = angular.isFunction;
   isElement   = angular.isElement;
 })
-  .info({ angularVersion: '1.6.9' })
+  .info({ angularVersion: '1.6.10-build.5552+sha.5b7e4b4' })
   .directive('ngAnimateSwap', ngAnimateSwapDirective)
 
   .directive('ngAnimateChildren', $$AnimateChildrenDirective)
